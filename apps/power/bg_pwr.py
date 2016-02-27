27cc66d92e38a0535c930a5a72cf31162994b344
Jaya M <jayachandranm@gmail.com>
2016-02-27 20:43:07 +0800

    This code runs, shows all parameters and writes the JSON file. The results have to be verified.

--- apps/power/bg_pwr.py
+++ apps/power/bg_pwr.py
@@ -70,7 +70,8 @@ pwrvals = {}
 
 date = [1002, 1816, 0300]
 
-client = ModbusClient(method="rtu", port="/dev/ttyRPC0", baudrate=19200, stopbits=1, bytesize=8, parity='E')
+client = ModbusClient(method="rtu", port="/dev/ttyAMA0", baudrate=19200, stopbits=1, bytesize=8, parity='E')
+#client = ModbusClient(method="rtu", port="/dev/ttyRPC0", baudrate=19200, stopbits=1, bytesize=8, parity='E')
 # client= ModbusClient(method = "rtu", port="/dev/ttyRPC0",baudrate= 19200,stopbits=1,bytesize=8,parity='E')
 print client.connect()
 
@@ -124,7 +125,7 @@ for item in range(0, 22):
     else:
         result_val = (val)[0] * 0.1
 
-    print result_val
+    print item, paramName, result_val
     pwrvals[paramName] = result_val
 
     with open('data.json', 'w') as f:
@@ -132,18 +133,19 @@ for item in range(0, 22):
 
 
     # For the last one, will skip Freq, as freq is separately handled.
-    add_hex2(reg_addr, 0x2)
+    #add_hex2(reg_addr, 0x2)
+    reg_addr = reg_addr + 0x2
 
     print " "
     #time.sleep(5)
 
-    paramName = paramNameList[22]
-    result = client.read_holding_registers(0x5B2C, 1, unit=0x01)
-    val = struct.unpack('>l', result.registers)[0]
-    #result_val = result.registers[0] * 0.01
-    result_val = val * 0.01
-    #        print "Frequency", int(v1), "Hz"
-    pwrvals[paramName] = result_val
+paramName = paramNameList[22]
+result = client.read_holding_registers(0x5B2C, 1, unit=0x01)
+#val = struct.unpack('>l', result.registers)[0]
+result_val = result.registers[0] * 0.01
+#result_val = val * 0.01
+print "Frequency", int(result_val), "Hz"
+pwrvals[paramName] = result_val
 
 
 for item in range(22, 40):
@@ -153,25 +155,32 @@ for item in range(22, 40):
     result = client.read_holding_registers(reg_addr, 1, unit=0x01)
     #print "Voltage", result.registers
     if item > 36: # unsigned
-        raw = struct.pack('>HH', result.registers[0], result.registers[1])
-        val = struct.unpack('>l', raw)
+        #raw = struct.pack('>H', result.registers[0])
+        #raw = struct.pack('>HH', result.registers[0], result.registers[1])
+        val = result.registers[0] 
+        #val = struct.unpack('>l', raw)
     else: # signed
-        raw = struct.pack('>HH', result.registers[0], result.registers[1])
-        val = struct.unpack('>l', raw)
+        #raw = struct.pack('>H', result.registers[0])
+        #raw = struct.pack('>HH', result.registers[0], result.registers[1])
+        val = result.registers[0] 
+        #val = struct.unpack('>l', raw)
     # fval = result.registers[1] * 0.1
     result_val = 0.0
     if (item < 33): #  or (item > 22)
-        result_val = (val)[0] * 0.1
+        #result_val = (val)[0] * 0.1
+        result_val = val * 0.1
     else:
-        result_val = (val)[0]
+        result_val = val 
+        #result_val = (val)[0]
 
-    print result_val
+    print item, paramName, result_val
     pwrvals[paramName] = result_val
 
     with open('data.json', 'w') as f:
         json.dump(pwrvals, f)
 
-    add_hex2(reg_addr, 0x1)
+    #add_hex2(reg_addr, 0x1)
+    reg_addr = reg_addr + 0x2
 
     print " "
 
