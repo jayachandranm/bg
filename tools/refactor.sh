e27afdc628c5b6ac7ce54df751af7292ce2c7897
Jaya <mjay@i2r.a-star.edu.sg>
2016-03-12 14:00:13 +0800

    The refactor script was missing from the commit. Added.

---
+++ tools/refactor.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# This script reconfigures the directories and updates sw to the new
+# versions.
+#
+######################################################################
+
+RUN_DIR="/home/arkbg/dev"
+SW_DIR="$RUN_DIR/devsrc"
+BKUP_DIR="$RUN_DIR/bkup"
+LOG_DIR="$RUN_DIR/logs"
+
+# Do not allow the script to run as root.  
+if [ "$(id -u)" == "0" ]; then
+    echo "This script cannot run as root"
+    exit 1
+fi
+
+# Remove all existing files. Prepare new dir for src.
+cd $RUN_DIR
+# Remove the existing repo.
+rm -rf github
+# Some files may have root permissions. Change all to user perm.
+chown -R arkbg.pi *
+#
+if [[ ! -e $SW_DIR ]]; then
+  mkdir -p $SW_DIR
+elif [[ ! -d $SW_DIR ]]; then
+  echo "$SW_DIR already exists but is not a directory" 1>&2
+fi
+#
+if [[ ! -e $BKUP_DIR ]]; then
+  mkdir -p $BKUP_DIR
+elif [[ ! -d $BKUP_DIR ]]; then
+  echo "$BKUP_DIR already exists but is not a directory" 1>&2
+fi
+#
+if [[ ! -e $LOG_DIR ]]; then
+  mkdir -p $LOG_DIR
+elif [[ ! -d $LOG_DIR ]]; then
+  echo "$LOG_DIR already exists but is not a directory" 1>&2
+fi
+
+function clone_master() {
+    echo "Cloning $1"
+    cd "$SW_DIR" \
+        && git clone "https://github.com/jayachandranm/$1"
+}
+
+clone_master "bgsense"
