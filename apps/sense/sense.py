9ce1c134e6fcffe57f4867e1544535d8177e1939
Jaya <mjay@i2r.a-star.edu.sg>
2016-03-17 14:32:36 +0800

    Updated sense code to save python objects to file during network failures, sendfile to read these objects and upload later. Both codes are expected to run through cron. Did additional code cleanup, and removed _2 extensions from filenames. Not tested.

--- apps/sense/sense.py
+++ apps/sense/sense.py
@@ -7,8 +7,10 @@ import json
 import serial
 import os
 from pi_sht1x import SHT1x
+import cPickle as pickle
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11,GPIO.OUT,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(11, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
 GPIO.setwarnings(False)
 dataPin = 11
 clkPin = 7
@@ -17,135 +19,133 @@ print "Serial Connected!"
 ser.flushInput()
 time.sleep(1)
 bus = smbus.SMBus(1)
-#config params
-loop_time=9.995
-num_samp_avg=6
-dev_ID="D1004"
 
-templ=[]
-humdl=[]
-cotol=[]
-luml=[]
+# config params
+loop_time = 9.995
+num_samp_avg = 6
+dev_ID = "D1004"
+BACKLOG_BUFF_LEN = 59
+#SERVER_ADDR = "172.18.53.42:81"
+SERVER_ADDR = "52.74.191.39"
+RUN_DIR = "/home/arkbg/dev/"
 
-tofile=[]
+templ = []
+humdl = []
+cotol = []
+luml = []
+
+payload = {}
+network_status = 1
 
 while 1:
-    #Check the sensor sampling interval value
-#    r = requests.put("http://172.18.53.42:81/BluIEQ/stationstatus.php", data="D1004")   	
-#    r = requests.put("http://52.74.191.39/BluIEQ/stationstatus.php", data="D1004")
-#    print r.status_code
-#    print (r.content)
-#    data = json.loads(r.content)
-#    if data[0]==None:
-#        delay = 60
-#         print "Default delay"
-#    else:
-#        delay = float(data[0])
-#    print "first",delay
 
-#Checking temp buffer and sending back to network
+    # Reading Sensor Values
+    for k in range(0, num_samp_avg):
+        try:
+            with SHT1x(11, 7, gpio_mode=GPIO.BOARD) as sensor:
+                templ.append(sensor.read_temperature())  # This is temperature value
+                print "Temperature = " + str(templ[k]) + " deg C"
+                humdl.append(sensor.read_humidity(templ[k]))  # This is humidity value
+                print "Humidity = " + str(humdl[k]) + " %"
+        except:
+            print "STH Error"
+        try:
+            ser.write("\xFE\x44\x00\x08\x02\x9F\x25")
+            time.sleep(.01)
+            resp = ser.read(7)
+            high = ord(resp[3])
+            low = ord(resp[4])
+            cotol.append((high * 256) + low)  # This is Co2 value
+            print "Co2 = " + str(cotol[k]) + " ppm"
+        except:
+            print "Co2 Error"
+        try:
+            data = bus.read_i2c_block_data(0x23, 0x11)
+            luml.append((data[1] + (256 * data[0])) / 1.2)  # This is luminance value
+            print "Luminosity " + str(luml[k]) + " lux"
+        except:
+            print "Luminance  Error"
 
-    if (len(tofile))>0:
-	
-	print "Network connection re-established ; Send data buffer to server"
-    	try:
-#       	r1 = requests.put("http://172.18.53.42:81/BluIEQ/sensordata.php", data=json.dumps(payload), timeout=0.1)
-        	r1 = requests.put("http://52.74.191.39/BluIEQ/sensordata.php", data=json.dumps(tofile), timeout=0.1)
-        	print r1.status_code
-        	print r1.content
-		del tofile[:]
-    	except:
-        	print "Network Failed Error:"
-#Reading Sensor Values	
-	
-    for k in range(0,num_samp_avg):
-	try:        
-		with SHT1x(11,7, gpio_mode=GPIO.BOARD) as sensor:
-            		templ.append(sensor.read_temperature())  # This is temperature value
-            		print "Temperature = "+str(templ[k])+" deg C"
-            		humdl.append(sensor.read_humidity(templ[k])) # This is humidity value
-            		print "Humidity = "+str(humdl[k])+" %"
-	except:
-        	print "STH Error"
-	try:
-        	ser.write("\xFE\x44\x00\x08\x02\x9F\x25")
-        	time.sleep(.01)
-        	resp = ser.read(7)
-        	high = ord(resp[3])
-        	low = ord(resp[4])
-        	cotol.append((high*256) + low)  # This is Co2 value
-        	print "Co2 = " + str(cotol[k])+" ppm"
-        except:
-                print "Co2 Error"
-	try:
-		data = bus.read_i2c_block_data(0x23,0x11)
-        	luml.append((data[1] + (256 * data[0])) / 1.2) #This is luminance value
-        	print "Luminosity " + str(luml[k])  + " lux"
-        except:
-                print "Luminance  Error"
-	
-	time.sleep(loop_time)
-	print " "
-    
+        time.sleep(loop_time)
+        print " "
+
     print "Averaging for 1 min"
     print "Length = " + str(len(templ))
-	
-    temp=(sum(templ)/len(templ))
-    print "Temperature = "+str(temp)+" deg C"
-    humd=(sum(humdl)/len(humdl))
-    print "Humidity = "+str(humd)+" %"
-    coto=(sum(cotol)/len(cotol))
-    print "Co2 = " + str(coto)+" ppm"
-    lum=(sum(luml)/len(luml))
-    print "Luminosity " + str(lum)  + " lux"
-    #Completed all sensor values
+
+    temp = (sum(templ) / len(templ))
+    print "Temperature = " + str(temp) + " deg C"
+    humd = (sum(humdl) / len(humdl))
+    print "Humidity = " + str(humd) + " %"
+    coto = (sum(cotol) / len(cotol))
+    print "Co2 = " + str(coto) + " ppm"
+    lum = (sum(luml) / len(luml))
+    print "Luminosity " + str(lum) + " lux"
+
+    # Completed all sensor values
     # Reset the lists
     del templ[:]
     del humdl[:]
     del cotol[:]
-    del luml[:]	 	
-    i = time.strftime("%Y-%m-%d %H:%M:%S")
-#    i = time.strftime("%Y-%m-%d %H:%M")
-    print (i)
-    payload = {"deviceID":dev_ID, "tempsensor": temp,"lumnsensor": lum, "humdsensor":humd, "cotosensor":coto, "time":i}
-    print "Send Data to server"
-#    try:	
-#    	r1 = requests.put("http://172.18.53.42:81/BluIEQ/sensordata.php", data=json.dumps(payload), timeout=0.1)
-#	r1 = requests.put("http://52.74.191.39/BluIEQ/sensordata.php", data=json.dumps(payload), timeout=0.1)
-#    	print r1.status_code
-#    	print r1.content
-#    except:
-#	print "Network Failed Error:"
+    del luml[:]
 
-#The value is always appeneded to the list. Whenever there is network, it will be sent. 
+    curr_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    #payload = {"deviceID": dev_ID, "tempsensor": temp, "lumnsensor": lum, "humdsensor": humd, "cotosensor": coto,
+    #           "time": i}
+    payload["deviceID"] = dev_ID
+    payload["tempsensor"] = temp
+    payload["lumnsensor"] = lum
+    payload["humdsensor"] = humd
+    payload["cotosensor"] = coto
+    payload["time"] = curr_time
 
-    tofile.append(payload)
-    if len(tofile)>255:
-	dir_name= "/home/arkbg/dev/"+time.strftime("%Y-%m-%d")
-#	print dir_name
+    print curr_time + " -> Send Data to server: " + json.dumps(payload)
+    try:
+         svc_url = "http://" + SERVER_ADDR + "/BluIEQ/sensordata.php"
+         r1 = requests.put(svc_url, data=json.dumps(payload), timeout=0.1)
+         print r1.status_code
+         #print r1.content
+    except:
+        print "Network Failed Error:"
+        network_status = 0
 
-	try:
-		os.makedirs(dir_name)
-		print "Directory Created" 
-		
-	except OSError:
-		if os.path.exists(dir_name):
-			print "Already directory exists"
-		else:
-			print "Some system Error in creating directory"
+    # If there was network failure, save this object to file.
+    # Another program will read the file and upload the data.
+    if network_status == 0:
+        dir_name = RUN_DIR + time.strftime("%Y-%m-%d")
+        #	print dir_name
 
-		print "Failed creating the directory"
+        try:
+            # A new directory for a new day.
+            os.makedirs(dir_name)
+            print "Directory Created"
 
-	print "Make the file"
-	try:
-		base_filename=time.strftime("%H_%M_%S")
-		abs_file_name=os.path.join(dir_name, base_filename + "." + "txt")
-		print abs_file_name
-		f = open(abs_file_name, 'w')
-		for item in tofile:
-			print>>f, item
-		del tofile[:]
-	except:  	
-		print "Failed to create file"
-#    time.sleep(delay)
+        except OSError as x:
+            print "Directory not created, err: " + x
+            if os.path.exists(dir_name):
+                print "Already directory exists"
+            else:
+                print "Some system Error in creating directory"
+
+        # For the same hour, append to existing file. New file for new hour.
+        base_filename = "local_" + time.strftime("%Y-%m-%d_%H_%M")
+        ext = ".current"
+        abs_file_name = os.path.join(dir_name, base_filename) + ext
+        print abs_file_name
+        # If the file does not exist, this data will be written in new file.
+        # Rename any .current files to .dat
+        try:
+            if not os.path.exists(abs_file_name):
+                from glob import glob
+                from os import rename
+                os.chdir(dir_name)
+                for file in glob.glob("*.current"):
+                    print(file)
+                    rename(file, file.replace(".current", ".dat", 1))
+
+            localfile = open(abs_file_name, 'wa')
+        except Exception as x:
+            print "File opreration error: " + x
+
+        pickle.dump(payload, localfile, pickle.HIGHEST_PROTOCOL)
+#
 GPIO.cleanup()
