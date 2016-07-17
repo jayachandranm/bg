8ab276faafff4808ae49f2c79d652fde011968a4
Jaya M <jayachandranm@gmail.com>
2016-07-17 11:51:17 +0800

    Split files by function to multiple files. To be tested.

---
+++ apps/obd/assemble-obd-message.js
@@ -0,0 +1,56 @@
+var Concentrate = require("concentrate/index");
+
+var crc16 = require('crc-itu').crc16;
+
+module.exports.loginReply = loginReply;
+module.exports.heartBeatReply = heartBeatReply;
+
+var c = Concentrate();
+
+function loginReply(dcMsg) {
+    var devID = dcMsg.dev_id;
+    var dataToDev = c.uint16le('0x4040')
+    .uint16le('0x29')
+    .uint8('0x03')
+    .string(devID, 'hex')
+    .uint16be('0x9001')
+    .uint32le('0xffffffff')
+    .uint16('0x0')
+    .uint32le('1395277770') // TODO: send current time.
+    .copy();
+
+    var crcReply = crc16(dataToDev.result());
+
+    console.log(crcReply.toString(16));
+
+    var dataToDev2 = c.uint16(crcReply)
+    .uint16('0x0a0d')
+    .result();
+
+    console.log(dataToDev2);
+    c.reset();
+    return dataToDev2;
+}
+
+
+
+// Heart Beat.
+function heartBeatReply(dcMsg) {
+    var dataToDev = c.uint16le('0x4040')
+    .uint16le('0x1F')
+    .uint8('0x03')
+    .string(devID, 'hex')
+    .uint16be('0x9003')
+    .copy();
+
+    var crcReply = crc16(dataToDev.result());
+
+    console.log(crcReply.toString(16));
+
+    var dataToDev2 = c.uint16(crcReply)
+    .uint16('0x0a0d')
+    .result();
+
+    c.reset();
+    return dataToDev2;
+}
--- apps/obd/fleetServer.js
+++ apps/obd/fleetServer.js
@@ -2,12 +2,24 @@
 var net = require('net') //aby
 //var crc = require('crc')
 
+var decode = require('./decode');
+var assemble = require('./assemble-obd-message');
+var dbutil = require('./dbutil');
+
 var HOST = '127.0.0.1';
 var PORT = 8686;
 var count = 1;
 var respToOBD = new Buffer ('4040290004323133474c32303134303036363331000000000090010E64864121EECA3F2A53CA3F2A53263C');
+var buf = new Buffer('40407F000431333630303030303030310000000000000000001001C1F06952FDF069529C91110000000000698300000C0000000000036401014C00030001190A0D04121A1480D60488C5721800000000AF4944445F3231364730325F532056312E322E31004944445F3231364730325F482056312E322E31000000DF640D0A', 'hex');
+
 //4040290004323133474c32303134303036363331000000000090010E64864121EECA3F2A53CA3F2A53263C
 console.log('\n' + respToOBD +'\n');
+
+// TODO: for test.
+var dcMsg = decode.decodeMessage(buf);
+processMessage2(dcMsg);
+
+
 // Create a server instance, and chain the listen function to it
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
 // The sock object the callback function receives UNIQUE for each connection
@@ -15,25 +27,31 @@ net.createServer(function(sock) {
 
     // We have a connection - a socket object is assigned to the connection automatically
     console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-    
+
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function(data) {
-        
+
         //var base64str = new Buffer(data).toString('base64');
         var rawData = new Buffer(data); //aby
         console.log('rawData: ' + rawData + '\n'); // aby
-        
+
         var base64str = new Buffer(data).toString('hex');
         //console.log('DATA ' + sock.remoteAddress + ': ' + data);
         console.log('DATA(base64) ' + sock.remoteAddress + ': ' + base64str);
-    
+
+        var dcMsg = decode.decodeMessage(rawData);
+        processMessage(dcMsg);
+
         // Write the data back to the socket, the client will receive it as data from the server
         sock.write('You said "' + data + '"');
+        // Send only once.
+        /*
         if(count == 1)
         {
             sock.write(respToOBD);
             count --;
         }
+        */
         //sock.pipe(sock);
     });
 
@@ -49,3 +67,53 @@ net.createServer(function(sock) {
 }).listen(PORT);//, HOST);
 
 console.log('Server listening on ' + HOST +':'+ PORT);
+
+function processMessage(sock, dcMsg) {
+  switch(dcMsg.type) {
+      case 0x1001:
+          console.log('Login Message from OBD.');
+          var replyMsg = assemble.loginReply(dcMsg);
+          sock.write(replyMsg);
+          dbutil.updateDB(dcMsg.payload.gps_data);
+          //console.log("DB updated.");
+          // TODO: Login reply.
+          break;
+      case 0x1003:
+          console.log('HeartBeatPackage received, send reply.')
+          var heartReply = assemble.heartBeatReply(dcMsg);
+          sock.write(heartReply);
+          break;
+      case 0x4001:
+          console.log('GPS data');
+          dbutil.updateDB(dcMsg.payload.gps_data);
+      case 0x4004:
+          console.log("Data Flow");
+  }
+
+
+
+// TODO: for test
+function processMessage2(dcMsg) {
+    switch(dcMsg.type) {
+        case 0x1001:
+            console.log('Login Message from OBD.');
+            var replyMsg = assemble.loginReply(dcMsg);
+            console.log(replyMsg);
+            //sock.write(replyMsg);
+            dbutil.updateDB(dcMsg.payload.gps_data);
+            //console.log("DB updated.");
+            // TODO: Login reply.
+            break;
+        case 0x1003:
+            console.log('HeartBeatPackage received, send reply.')
+            var heartReply = assemble.heartBeatReply(dcMsg);
+            //sock.write(heartReply);
+            break;
+        case 0x4001:
+            console.log('GPS data');
+            dbutil.updateDB(dcMsg.payload.gps_data);
+        case 0x4004:
+            console.log("Data Flow");
+    }
+
+}
