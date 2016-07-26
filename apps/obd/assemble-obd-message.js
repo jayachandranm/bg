6855d35067391075d8de22e0ee4e7a70a5ae8bd7
Jaya <jayachandranm@gmail.com>
2016-07-26 17:42:55 +0800

    Added template code for all messages from MO to MT, server to device.

--- apps/obd/assemble-obd-message.js
+++ apps/obd/assemble-obd-message.js
@@ -32,8 +32,6 @@ function loginReply(dcMsg) {
     return dataToDev2;
 }
 
-
-
 // Heart Beat.
 function heartBeatReply(dcMsg) {
     var devID = dcMsg.dev_id;
@@ -54,4 +52,73 @@ function heartBeatReply(dcMsg) {
 
     c.reset();
     return dataToDev2;
+}
+
+function setMessage(dcMsg) {
+  // 0x2001
+  // TODO: Genereate the number based on some logic.
+  var cmdSeq = 12345;
+  // TODO: Fix these.
+  var msgSize = '0x1F';
+  var devID = '0x32320000000000000000000000';
+  var tlvCount = '11';
+  //
+  var dataToDev = c.uint16le('0x4040')
+  .uint16le(msgSize)
+  .uint8('0x03')
+  .string(devID, 'hex')
+  .uint16be('0x2001')
+  .uint16le(cmdSeq)
+  .uint8(tlvCount)
+  // TODO: TLV array.
+  .copy();
+
+  var crcReply = crc16(dataToDev.result());
+  var dataToDev2 = c.uint16(crcReply)
+  .uint16('0x0a0d')
+  .result();
+  //
+  c.reset();
+  return dataToDev2;
+}
+
+function queryMessage(dcMsg) {
+  // 0x2002
+}
+
+function locationQuery(dcMsg) {
+  // 0x3001
+}
+
+function clearDTC(dcMsg) {
+  // 0x3002
+}
+
+function restoreFactorySettings(dcMsg) {
+  // 0x3003
+}
+
+function sendTextInfo(dcMsg) {
+  // TODO: Two type codes.
+  // 0x3006, 0xB006
+}
+
+function alarmConfirmation(dcMsg) {
+  // 0xC007
+}
+
+function driverIdConfirmation(dcMsg) {
+  // 0xC00C
+}
+
+function updateNotification(dcMsg) {
+  // 0x5001
+}
+
+function updateMessage(dcMsg) {
+  // 0x5002
+}
+
+function agpsMessage(dcMsg) {
+  // 0x5102
 }
