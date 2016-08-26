dc377338018c2de2eecc79809dec1456f733a2de
Jaya <mjay@i2r.a-star.edu.sg>
2016-08-26 18:05:06 +0800

    Performed code reformatting.

--- apps/web/drupal/modules/bgmap/bgmap.js
+++ apps/web/drupal/modules/bgmap/bgmap.js
@@ -204,73 +204,73 @@
         // Clear the array before getting new values.
         latlngs.length = 0;
         console.log('Trace: Ajax call: ', startTime, endTime);
-          var postData = {};
-          postData['reqtype'] = 'trc';
-          var filter = {};
-          filter['sidList'] = [sid];
-          filter['start'] = startTime;
-          filter['end'] = endTime; // or current time.
-          postData['filter'] = filter;
-          jsonPost = JSON.stringify(postData);
-         //post_url = basepath + '?q=bgmap/geoj/' + 'trc';
-          //post_url = basepath + '?q=bgmap/geoj';
-          //console.log(post_url, jsonPost);
+        var postData = {};
+        postData['reqtype'] = 'trc';
+        var filter = {};
+        filter['sidList'] = [sid];
+        filter['start'] = startTime;
+        filter['end'] = endTime; // or current time.
+        postData['filter'] = filter;
+        jsonPost = JSON.stringify(postData);
+        //post_url = basepath + '?q=bgmap/geoj/' + 'trc';
+        //post_url = basepath + '?q=bgmap/geoj';
+        //console.log(post_url, jsonPost);
         /*
-                 + sid + '/'
-                 + startTime + '/'
-                 + endTime;
+        + sid + '/'
+        + startTime + '/'
+        + endTime;
         */
-          post_url = basepath + '?q=bgmap/geoj';
-           $.ajax({
-              url: post_url,
-              success: function(jsonData) {
-                console.log(jsonData);
-              },
-              complete: function() {
-                 //setTimeout(requestData, 2000);
-                 console.log('success');
-              },
-              //error: function(xhr, status, error) {
-              error: function() {
-                console.log('Error loading ');
-              }
-            }); // ajax
-
-/*
+        post_url = basepath + '?q=bgmap/geoj';
         $.ajax({
           url: post_url,
-          type: 'GET',
-          dataType: 'json',
-          //data: jsonPost,
-          data: {test : 123 },
-          success: function (jsonData) {
-            console.log('Received JSON for Trace=', jsonData);
-            for (var i = 0; i < jsonData.length; i++) {
-              latlngs.push([parseFloat(jsonData[i].lt), parseFloat(jsonData[i].lg)]);
-            }
-            console.log(latlngs);
-            //var test = JSON.stringify(latlngs);
-            //var test2 = [[1.46, 103.83], [1.45, 103.82], [1.43, 103.81]];
-            //console.log(test);
-            polylines = L.polyline(latlngs, { color: 'blue' });
-            polylines.addTo(map2);
-            //map2.fitBounds(latlngs);
-            //var polygon = L.polygon().addTo(map);
+          success: function(jsonData) {
+            console.log(jsonData);
           },
-          complete: function () {
-            //setTimeout(requestTraceData, 2000);
+          complete: function() {
+            //setTimeout(requestData, 2000);
+            console.log('success');
           },
           //error: function(xhr, status, error) {
-          error: function (xhr, status, error) {
-            console.log("Error in Ajax call", xhr, status, error);
-            //alert('Error loading ');
+          error: function() {
+            console.log('Error loading ');
           }
         }); // ajax
-*/
-      }); // requestTraceData
 
-      requestTraceData();
-    } // if settings, trace.
-  } // attach
+        /*
+        $.ajax({
+        url: post_url,
+        type: 'GET',
+        dataType: 'json',
+        //data: jsonPost,
+        data: {test : 123 },
+        success: function (jsonData) {
+        console.log('Received JSON for Trace=', jsonData);
+        for (var i = 0; i < jsonData.length; i++) {
+        latlngs.push([parseFloat(jsonData[i].lt), parseFloat(jsonData[i].lg)]);
+      }
+      console.log(latlngs);
+      //var test = JSON.stringify(latlngs);
+      //var test2 = [[1.46, 103.83], [1.45, 103.82], [1.43, 103.81]];
+      //console.log(test);
+      polylines = L.polyline(latlngs, { color: 'blue' });
+      polylines.addTo(map2);
+      //map2.fitBounds(latlngs);
+      //var polygon = L.polygon().addTo(map);
+    },
+    complete: function () {
+    //setTimeout(requestTraceData, 2000);
+  },
+  //error: function(xhr, status, error) {
+  error: function (xhr, status, error) {
+  console.log("Error in Ajax call", xhr, status, error);
+  //alert('Error loading ');
+}
+}); // ajax
+*/
+}); // requestTraceData
+
+requestTraceData();
+} // if settings, trace.
+} // attach
 } // behaviors, bgmap
 })(jQuery);
