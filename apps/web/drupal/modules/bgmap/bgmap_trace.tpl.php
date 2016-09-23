e1ce533928f8878ad905be7cb3ebc660c5e8e1b4
Jaya-AWS <jayachandranm@gmail.com>
2016-09-23 11:51:38 +0000

    Experimenting with block options. Used prefix and suffix for separating div wrappers from content while using #attached. Created a template file for theming the block content instead of embedding inside the block_view hook. Tested for trace block. Both tested to be working.

---
+++ apps/web/drupal/modules/bgmap/bgmap_trace.tpl.php
@@ -0,0 +1,9 @@
+<div class='row'> 
+    <div class='col-md-6' style='padding-bottom:1em; padding-top:1em'>
+       <input class='form-control' class='pull-left' type='text' name='daterange' value='01/01/2015 1:30 PM - 01/01/2015 2:00 PM'> 
+     </div> 
+</div> 
+<div class='row' style='margin-top:1em' id='show_map2'>
+    Map will display here.....
+</div>
+
