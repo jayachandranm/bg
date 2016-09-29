a18b3e655cc7578ca8d1626142778c4c0d2dcfd2
Jaya-AWS <jayachandranm@gmail.com>
2016-09-29 08:54:28 +0000

    (1) Added sample code in template.php file for customizing blocks created by a module, inside a theme.  (2) Updated node template file to be based on bootstrap node template file. Fixed some issues in printing as well. (3) Added example for using JS file in theme for hide/show blocks or other animations.

--- apps/web/drupal/themes/bgtrans/templates/node--vehicle.tpl.php
+++ apps/web/drupal/themes/bgtrans/templates/node--vehicle.tpl.php
@@ -1,63 +1,112 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display a node.
+ *
+ * Available variables:
+ * - $title: the (sanitized) title of the node.
+ * - $content: An array of node items. Use render($content) to print them all,
+ *   or print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $user_picture: The node author's picture from user-picture.tpl.php.
+ * - $date: Formatted creation date. Preprocess functions can reformat it by
+ *   calling format_date() with the desired parameters on the $created variable.
+ * - $name: Themed username of node author output from theme_username().
+ * - $node_url: Direct URL of the current node.
+ * - $display_submitted: Whether submission information should be displayed.
+ * - $submitted: Submission information created from $name and $date during
+ *   template_preprocess_node().
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - node: The current template type; for example, "theming hook".
+ *   - node-[type]: The current node type. For example, if the node is a
+ *     "Blog entry" it would result in "node-blog". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - node-teaser: Nodes in teaser form.
+ *   - node-preview: Nodes in preview mode.
+ *   The following are controlled through the node publishing options.
+ *   - node-promoted: Nodes promoted to the front page.
+ *   - node-sticky: Nodes ordered above other non-sticky nodes in teaser
+ *     listings.
+ *   - node-unpublished: Unpublished nodes visible only to administrators.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * Other variables:
+ * - $node: Full node object. Contains data that may not be safe.
+ * - $type: Node type; for example, story, page, blog, etc.
+ * - $comment_count: Number of comments attached to the node.
+ * - $uid: User ID of the node author.
+ * - $created: Time the node was published formatted in Unix timestamp.
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $zebra: Outputs either "even" or "odd". Useful for zebra striping in
+ *   teaser listings.
+ * - $id: Position of the node. Increments each time it's output.
+ *
+ * Node status variables:
+ * - $view_mode: View mode; for example, "full", "teaser".
+ * - $teaser: Flag for the teaser state (shortcut for $view_mode == 'teaser').
+ * - $page: Flag for the full page state.
+ * - $promote: Flag for front page promotion state.
+ * - $sticky: Flags for sticky post setting.
+ * - $status: Flag for published status.
+ * - $comment: State of comment settings for the node.
+ * - $readmore: Flags true if the teaser content of the node cannot hold the
+ *   main body content.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ *
+ * Field variables: for each field instance attached to the node a corresponding
+ * variable is defined; for example, $node->body becomes $body. When needing to
+ * access a field's raw values, developers/themers are strongly encouraged to
+ * use these variables. Otherwise they will have to explicitly specify the
+ * desired field language; for example, $node->body['en'], thus overriding any
+ * language negotiation rule that was previously applied.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see template_process()
+ *
+ * @ingroup templates
+ */
+?>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-    <?php if ($display_submitted || (module_exists('comment') && ($node->comment == COMMENT_NODE_OPEN || ($node->comment == COMMENT_NODE_CLOSED && $node->comment_count > 0)))): ?>
-    <div class="post-submitted-info">
-        <?php if ($display_submitted): ?>
-        <div class="submitted-date">
-          <?php $custom_month = format_date($node->created, 'custom', 'M'); ?>
-          <?php $custom_day = format_date($node->created, 'custom', 'd'); ?>
-          <?php $custom_year = format_date($node->created, 'custom', 'Y'); ?>
-          <i class="fa fa-calendar hidden-lg hidden-md hidden-sm"></i>
-          <div class="month"><?php print $custom_month; ?></div>
-          <div class="day"><?php print $custom_day; ?></div>
-          <div class="year"><?php print $custom_year; ?></div>
-        </div>
-        <?php endif;?>
-        <?php if (module_exists('comment') && ($node->comment == COMMENT_NODE_OPEN || ($node->comment == COMMENT_NODE_CLOSED && $node->comment_count > 0))): ?>
-        <div class="comments-count">
-          <i class="fa fa-comment"></i>
-          <div class="comment-counter"><?php print $node->comment_count; ?></div>
-        </div>
-        <?php endif;?>
-    </div>
-    <?php endif;?>
-    <?php if ($display_submitted || (module_exists('comment') && ($node->comment == COMMENT_NODE_OPEN || ($node->comment == COMMENT_NODE_CLOSED && $node->comment_count > 0)))) { ?>
-    <div class="node-main-content custom-width">
-    <?php } else { ?>
-    <div class="node-main-content full-width">
-    <?php } ?>
-    <?php if ($title_prefix || $title_suffix || $display_submitted || !$page): ?>
-    <header>
-      <?php print render($title_prefix); ?>
-      <?php if (!$page): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-
-      <?php if ($display_submitted): ?>
-          <div class="submitted-user">
-          <?php print t('By !username', array('!username' => $name)); ?>
-          </div>
-      <?php endif; ?>
-        
-      <?php print $user_picture; ?> 
-
-    </header>
+  <?php if ((!$page && !empty($title)) || !empty($title_prefix) || !empty($title_suffix) || $display_submitted): ?>
+  <header>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page && !empty($title)): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
     <?php endif; ?>
-
-    <div class="content"<?php print $content_attributes; ?>>
-      <?php
-        // We hide the comments and links now so that we can render them later.
-        hide($content['comments']);
-        hide($content['links']);
-        // jaya commented.
-        //print render($content);
-      ?>
-<?php // jaya add, till render photo ?> 
+    <?php print render($title_suffix); ?>
+    <?php if ($display_submitted): ?>
+    <span class="submitted">
+      <?php print $user_picture; ?>
+      <?php print $submitted; ?>
+    </span>
+    <?php endif; ?>
+  </header>
+  <?php endif; ?>
+  <?php
+    // Hide comments, tags, and links now so that we can render them later.
+    hide($content['comments']);
+    hide($content['links']);
+    hide($content['field_tags']);
+    // jaya commented.
+    //print render($content);
+  ?>
+    <div class="row" style="padding-bottom:20px">
       <div class="col-md-3">
-      <?php if ($photo = render($content['field_vehicle_photo'])) { ?>
-        <?php /* print render($photo); */ ?>
+        <? // TODO: handle if exists ?>
         <?php print render($content['field_vehicle_photo']); ?>
-      <?php } ?>
       </div>
       <div class="col-md-9">
         <?php print render($content['field_vehicle_num']); ?>
@@ -65,23 +114,25 @@
         <?php print render($content['field_sector']); ?>
         <?php print render($content['field_driver']); ?>
         <?php print render($content['field_assistant']); ?>
+        <?php print render($content['og_group_ref']); ?>
         <?php /* echo $node->field_vehicle_num['und'][0]['value'];*/ ?>
         <?php /*echo $node->body['und'][0]['value'];*/ ?>
       </div>
       <div class="col-md-12">
-        <?php print render($content['body']); ?>
         <p>
+        <?php print render($content['body']); ?>
         <hr>
       </div>
     </div>
+  <?php
+    print render($content);
+  ?>
 
-    <?php if ($links = render($content['links'])): ?>
-    <footer>
+  <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
+  <footer>
+    <?php print render($content['field_tags']); ?>
     <?php print render($content['links']); ?>
-    </footer>
-    <?php endif; ?>
-
-    <?php print render($content['comments']); ?>
-  </div>
-
+  </footer>
+  <?php endif; ?>
+  <?php print render($content['comments']); ?>
 </article>
