56367a27b4e4ef76a9aa6b2eafc4076287fced29
Jaya-AWS <jayachandranm@gmail.com>
2016-09-30 08:40:06 +0000

    Moved the theme related template files and preprocess funtions to separate theme folder. Tested to be working.

---
+++ apps/web/drupal/modules/bgmap/theme/bgmap_trace.tpl.php
@@ -0,0 +1,21 @@
+<div class='row'> 
+    <div class='col-md-4' style='padding-bottom:1em; padding-top:1em'> 
+       <div class="inner-addon left-addon">
+           <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+           <input class='form-control' class='pull-left' type='text' name='daterange' value='01/01/2015 1:30 PM - 01/01/2015 2:00 PM' >  
+       </div> 
+    </div> 
+</div> 
+<!-- <div class='row' style='margin-top:1em' id='trace_map'>  -->
+    <div id='trace_map' style='margin-top:1em'>
+        Map will display here.....
+    </div>
+<!-- </div> -->
+
+<!--
+<div id="daterange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
+    <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;
+    <span></span> <b class="caret"></b>
+</div>
+-->
+
