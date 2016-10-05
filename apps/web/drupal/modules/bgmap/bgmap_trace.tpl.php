68cc88ccb97f8385169c4787827fe4a1275346de
Jaya <mjay@i2r.a-star.edu.sg>
2016-10-05 11:07:01 +0800

    Formatted both theme files and module files.

--- apps/web/drupal/modules/bgmap/bgmap_trace.tpl.php
+++ apps/web/drupal/modules/bgmap/bgmap_trace.tpl.php
@@ -1,15 +1,16 @@
-<div class='row'> 
-    <div class='col-md-4' style='padding-bottom:1em; padding-top:1em'> 
-       <div class="inner-addon left-addon">
-           <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
-           <input class='form-control' class='pull-left' type='text' name='daterange' value='01/01/2015 1:30 PM - 01/01/2015 2:00 PM' >  
-       </div> 
-    </div> 
-</div> 
-<!-- <div class='row' style='margin-top:1em' id='trace_map'>  -->
-    <div id='trace_map' style='margin-top:1em'>
-        Map will display here.....
+<div class='row'>
+    <div class='col-md-4' style='padding-bottom:1em; padding-top:1em'>
+        <div class="inner-addon left-addon">
+            <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+            <input class='form-control' class='pull-left' type='text' name='daterange'
+                   value='01/01/2015 1:30 PM - 01/01/2015 2:00 PM'>
+        </div>
     </div>
+</div>
+<!-- <div class='row' style='margin-top:1em' id='trace_map'>  -->
+<div id='trace_map' style='margin-top:1em'>
+    Map will display here.....
+</div>
 <!-- </div> -->
 
 <!--
--- apps/web/drupal/themes/bgtrans/template.php
+++ apps/web/drupal/themes/bgtrans/template.php
@@ -4,57 +4,61 @@
  * The primary PHP file for this theme.
  */
 
-function bgtrans_theme() {
-  $items = array();
-  // create custom user-login.tpl.php
-  $items['user_login'] = array(
-  'render element' => 'form',
-  'path' => drupal_get_path('theme', 'bgtrans') . '/templates',
-  'template' => 'user-login',
-  'preprocess functions' => array(
-     'bgtrans_preprocess_user_login'
-  ),
- );
- //
- $items['vehicle_node_form'] = array(
-  'arguments' => array('form' => NULL),
-  'render element' => 'form',
-  'path' => drupal_get_path('theme', 'bgtrans') . '/templates',
-  'template' => 'vehicle-node-form',
-  //'template' => 'templates/custom-content-type-node-form',
-   
- );
- //print_r($items);
- dpm($items);
-return $items;
+function bgtrans_theme()
+{
+    $items = array();
+    // create custom user-login.tpl.php
+    $items['user_login'] = array(
+        'render element' => 'form',
+        'path' => drupal_get_path('theme', 'bgtrans') . '/templates',
+        'template' => 'user-login',
+        'preprocess functions' => array(
+            'bgtrans_preprocess_user_login'
+        ),
+    );
+    //
+    $items['vehicle_node_form'] = array(
+        'arguments' => array('form' => NULL),
+        'render element' => 'form',
+        'path' => drupal_get_path('theme', 'bgtrans') . '/templates',
+        'template' => 'vehicle-node-form',
+        //'template' => 'templates/custom-content-type-node-form',
+
+    );
+    //print_r($items);
+    dpm($items);
+    return $items;
 }
 
-function bgtrans_preprocess_user_login(&$vars) {
-  $vars['intro_text'] = t(' Login');
+function bgtrans_preprocess_user_login(&$vars)
+{
+    $vars['intro_text'] = t(' Login');
 }
 
 /*
  *  Remove labels and add HTML5 placeholder attribute to login form
  */
-function bgtrans_form_alter(&$form, &$form_state, $form_id) {
-  //dpm($form);
-  if ( TRUE === in_array( $form_id, array( 'user_login', 'user_login_block') ) ) {
-    $form['name']['#attributes']['placeholder'] = t( 'Username / Email' );
-    $form['pass']['#attributes']['placeholder'] = t( 'Password' );
-    $form['name']['#title_display'] = "invisible";
-    $form['pass']['#title_display'] = "invisible";
-    //$form['actions']['submit']['#value'] = t('add desired text for submit button here');
-  }
-  if ($form_id == 'vehicle_node_form') {
+function bgtrans_form_alter(&$form, &$form_state, $form_id)
+{
     //dpm($form);
-  }
+    if (TRUE === in_array($form_id, array('user_login', 'user_login_block'))) {
+        $form['name']['#attributes']['placeholder'] = t('Username / Email');
+        $form['pass']['#attributes']['placeholder'] = t('Password');
+        $form['name']['#title_display'] = "invisible";
+        $form['pass']['#title_display'] = "invisible";
+        //$form['actions']['submit']['#value'] = t('add desired text for submit button here');
+    }
+    if ($form_id == 'vehicle_node_form') {
+        //dpm($form);
+    }
 }
 
 
 /*
  *  Remove login form descriptions
  */
-function bgtrans_form_user_login_alter(&$form, &$form_state) {
+function bgtrans_form_user_login_alter(&$form, &$form_state)
+{
     $form['name']['#description'] = t('');
     $form['pass']['#description'] = t('');
 }
