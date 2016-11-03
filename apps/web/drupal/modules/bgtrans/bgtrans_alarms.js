3d6f1280f6dc347a85eb48a36c759f9ebaf7f5cf
Jaya-AWS <jayachandranm@gmail.com>
2016-11-03 08:55:26 +0000

    Updated both PHP and JS files to achieve the required overall flow. The rendering of data after Ajax is TBD. Code not tested.

--- apps/web/drupal/modules/bgtrans/bgtrans_alarms.js
+++ apps/web/drupal/modules/bgtrans/bgtrans_alarms.js
@@ -1,11 +1,11 @@
 (function ($) {
     Drupal.behaviors.bgtrans = {
         attach: function (context, settings) {
-            console.log('JS attach for RT, initialization.');
-            if (Drupal.settings.rt) {
+            console.log('JS attach for Alarms, initialization.');
+            if (Drupal.settings.alarms) {
                 // No context parameters are required.
                 var sid2vehmap = {};
-                var veh_list = Drupal.settings.rt.veh_list;
+                var veh_list = Drupal.settings.alarms.veh_list;
                 sid2vehmap = veh_list;
                 var num_veh = veh_list.length;
                 var sid_list = [];
@@ -22,58 +22,30 @@
                 console.log('Retrieving bgtrans settings.');
                 //
                 // TODO: Do this in a theme.
-                $("#block-bgtrans-rtsingle").height(500);
-                $("#rt_map").height(400);
+                $("#block-bgtrans-alarms").height(500);
+                //$("#rt_alarms").height(400);
 
-/***  little hack starts here ***/
-L.Map = L.Map.extend({
-    openPopup: function(popup) {
-        //        this.closePopup();  // just comment this
-        this._popup = popup;
-
-        return this.addLayer(popup).fire('popupopen', {
-            popup: this._popup
-        });
-    }
-}); 
-/***  end of hack ***/
-
-                var map = new L.map('rt_map', {
-                              fullscreenControl: true,
-                              fullscreenControlOptions: {
-                                position: 'topleft'
-                              }
-                           });
-                // Default home location.
-                var lat = 1.421, lng = 103.829;
-                //center: [51.505, -0.09], zoom: 13
-                map.setView([lat, lng], 13);
-                //
-                var rtGeoJsonLayer = L.geoJson().addTo(map);
-
-                L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(map);
 
                 /*
                  * Real time updates.
                  */
-                var requestCurrentLoc = (function () {
-                    console.log('RT: Ajax call.');
+                var requestCurrentAlarms = (function () {
+                    console.log('Alarms: Ajax call.');
                     //post_url = basepath + '?q=bgtrans/geoj/' + 'rt';
-                    post_url = basepath + '?q=bgtrans/geoj';
+                    post_url = basepath + '?q=bgtrans/get/alarms';
                     var postData = {};
-                    postData['reqtype'] = 'rt';
+                    postData['reqtype'] = 'alarms';
                     var filter = {};
                     console.log(sid_list);
                     //var sid = veh_list.sid;
                     filter['sidList'] = sid_list;
-                    filter['start'] = -1;
-                    filter['end'] = -1; // or current time.
+                    var timenow = (new Date()).getTime();
+                    filter['start'] = timenow - (1*60*60*1000);
+                    filter['end'] = timenow; // or current time.
                     postData['filter'] = filter;
                     jsonPost = JSON.stringify(postData);
                     console.log(jsonPost);
-                    map.removeLayer(rtGeoJsonLayer);
+                    // Remove current table?
                     $.ajax({
                         url: post_url,
                         type: 'POST',
@@ -82,52 +54,11 @@ L.Map = L.Map.extend({
                         data: {jsonPost: jsonPost},
                         success: function (jsonData) {
                             //console.log('Received JSON for All Veh=', jsonData);
-                            console.log('Received JSON for RT Single=', JSON.stringify(jsonData));
-                            //rtGeoJsonLayer = L.geoJson().addTo(map);
-                            var pop;
-                            rtGeoJsonLayer = L.geoJson(jsonData, {
-                                pointToLayer: function (feature, latlng) {
-                                    map.panTo(latlng);
-                                    //layer.bindPopup(feature.properties.title);
-                                    return L.circleMarker(latlng, {
-                                        // Will be overwritten by style function below.
-                                        radius: 10,
-                                    });
-                                },
-                                style: function (feature) {
-                                    console.log(feature.properties.style);
-                                    return feature.properties.style;
-                                    //return {fillColor: "blue", color: "blue", fillOpacity: 0.5};
-                                },
-                                onEachFeature: function (feature, layer) {
-                                    if (feature.properties && feature.properties.title) {
-                                        var sid = feature.properties.title;
-                                        var nid = sid2vehmap[sid].nid;
-                                        var vnum = sid2vehmap[sid].vnum;
-                                        var nodeurl = basepath + '?q=node/' + nid;
-                                        var popContent = "<a href=" + nodeurl + ">" + vnum + "</a>";
-                                        var popup = L.popup({
-                                                closeButton: false
-                                                // className: 'popup'
-                                            }).setContent(popContent);
-                                        // TODO: have to be array of pops.
-                                        pop = layer.bindPopup(popup); 
-                                        //pop = layer.bindPopup(popContent); //.openPopup();
-                                        //layer.bindPopup(popContent).openOn(map);
-                                        //map.addLayer(popContent);
-                                    }
-                                }
-                            });
-                            //rtGeoJsonLayer.addData(jsonData);
-                            // TODO: if valid.
-                            rtGeoJsonLayer.addTo(map);
-                            // popup need map reference. Open only after adding the layer to map
-                            // TODO: if valid. Enable multiple pops.
-                            pop.openPopup();
+                            console.log('Received JSON for Alarms=', JSON.stringify(jsonData));
                         },
                         complete: function () {
                             console.log('Ajax processing complete, call again after delay');
-                            setTimeout(requestCurrentLoc, 50000);
+                            setTimeout(requestCurrentAlarms, 50000);
                         },
                         error: function (xhr, status, error) {
                             //error: function () {
@@ -136,7 +67,7 @@ L.Map = L.Map.extend({
                         }
                     }); // ajax
                 }); // requestData
-                requestCurrentLoc();
+                requestCurrentAlarms();
             } // if settings, bgtrans
         } // attach
     } // behaviors, bgtrans
