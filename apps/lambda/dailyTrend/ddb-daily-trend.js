c719aa141a7124d1997e7464efc056b74aba55f3
JM <jayachandranm@gmail.com>
2017-04-27 01:41:16 +0000

    Tested the code to be working locally. To be tested on Lambda.

--- apps/lambda/dailyTrend/ddb-daily-trend.js
+++ apps/lambda/dailyTrend/ddb-daily-trend.js
@@ -5,96 +5,107 @@ var aws = require('aws-sdk');
 //var zlib = require('zlib');
 var sids = require('./station-ids.json')
 
-var dateFormat = require('dateformat');
-var ts = dateFormat(new Date(), "mmddyyyy-HHMMss")
+//var dateFormat = require('dateformat');
+//var ts = dateFormat(new Date(), "mmddyyyy-HHMMss")
 
 aws.config.update({ region: 'ap-southeast-1' });
 //dynamo = new aws.DynamoDB();
-dynDoc = new aws.DynamoDB.DocumentClient();
+dynDocClient = new aws.DynamoDB.DocumentClient();
 
 
 var count = 0;
 var sidSize = sids.length;
 
-      function multiQuery() {
+var tablename = 'OBDTable_mmmYYYY';
+
+      var params = {
+        TableName: tablename,
+        //IndexName: 'Index',
+        KeyConditionExpression: 'obd_dev_id = :hkey and #ts BETWEEN :rkey_l AND :rkey_h',
+        ExpressionAttributeValues: {
+          ':hkey': sids[0],
+          ':rkey_l': 1480565971000,
+          ':rkey_h': 1480566618000
+        },
+        ExpressionAttributeNames: {
+          '#ts': 'timestamp',
+        },
+      };
+
+      function updateDailyTrend() {
         //console.log("Multi query..", self._count, self._sidSize, "===============");
         if(count < sidSize) {
           params.ExpressionAttributeValues = { ':hkey': sids[count], ':rkey_l': 1480565971000, ':rkey_h': 1480566618000 };
-          queryOne(params, function(err, data){
+          trend4sid(params, function(err, data){
             // if err..
             // if data..
-            var val = data;
-            addtoTrendTable(val, function(err, data){
+            //var avgVal = data;
+            var item = data;
+            item.device_id = sids[count];
+            addtoTrendTable(item, function(err){
               count++;
-              setTimeout(multiQuery, 1000);
+              setTimeout(updateDailyTrend, 1000);
             });
           });
         }
       }
 
 
-var queryOne = function (params, callback) {
+var trend4sid = function (params, callback) {
   // start streaminf table data
   var sum = 0.0;
-  dynDoc.query(params, function (err, data) {
+  dynDocClient.query(params, function (err, data) {
     var numItems = data.Items.length;
     if (err) {
       console.log(err, err.stack);
       callback(err);
     }
     else {
+      //console.log(data.Items[0].gps_data);
       for (var idx = 0; idx < data.Items.length; idx++) {
         var item = data.Items[idx];
-        var lat = item.gps_item.latitude;
+        var lat = item.gps_data.latitude;
         sum = sum + lat;
         //self._count++;
       }
+      var timestamp = data.Items[data.Items.length - 1].timestamp;
       var avg = sum / numItems;
-      callback(avg);
+      var item = { "timestamp": timestamp, "avg": avg };
+      callback(null, item);
     }
   });
 };
      
-
-
-
-
-/*
-  var dyno = Dyno({
-    table: 'OBDTable_mmmYYYY',
-    region: 'ap-southeast-1',
-    //endpoint: 'http://localhost:4567'
-  });
-*/
-//exports.handler = function (event, context) {
-function backupTable(context) {
-//function backupTable(tablename, callback) {
-  var tablename = 'OBDTable_mmmYYYY';
-
-  //var data_stream = dyno.scanStream();
-
+var addtoTrendTable = function (item, callback) {
+  var tablename = 'IoTTestDynDB';
   var params = {
-    //TableName: 'Table',
-    //IndexName: 'Index',
-    KeyConditionExpression: 'obd_dev_id = :hkey and #ts BETWEEN :rkey_l AND :rkey_h',
-    ExpressionAttributeValues: {
-      ':hkey': '213EP2016000570',
-      ':rkey_l': 1480565971000,
-      ':rkey_h': 1480566618000
-    },
-    ExpressionAttributeNames: {
-      '#ts': 'timestamp',
-    },
+    TableName:tablename,
+    Item: item
+/*
+    Item:{
+        "year": year,
+        "title": title,
+        "info":{
+            "plot": "Nothing happens at all.",
+            "rating": 0
+        }
+    }
+*/
   };
 
+  dynDocClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        callback(err);
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+        callback();
+    }
+  });
+}
 
+updateDailyTrend();
 
-  // body will contain the compressed content to ship to s3
-  //var body = data_stream.pipe(process.stdout);
+module.exports.updateDailyTrend = updateDailyTrend;
 
-    //send(function(err, data) { console.log(err, data); callback(); });
-} //function backupTable
-//};
-
-module.exports.backupTable = backupTable;
 
--- apps/lambda/dailyTrend/index.js
+++ apps/lambda/dailyTrend/index.js
@@ -1,5 +1,5 @@
 var process = require('./ddb-daily-trend');
 
 exports.handler = function (event, context) {
-  process.processDaily(context);
+  process.updateDailyTrend(context);
 };
