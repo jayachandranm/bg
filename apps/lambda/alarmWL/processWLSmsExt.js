fb93c27c94f7d2dd8d27b9bd68efb35da3f8586a
Jaya <jayachandranm@gmail.com>
2017-05-04 16:54:33 +0800

    Code tested to be working upto preparing message. Getting shadow moved back to main code, otherwise, the callback has to be handled properly.

--- apps/lambda/alarmWL/processWLSmsExt.js
+++ apps/lambda/alarmWL/processWLSmsExt.js
@@ -4,13 +4,16 @@ var AWS = require("aws-sdk");
 var http = require('http');
 var utils = require('./wlAlertUtils');
 var config = require('./config.json');
-var mqttmsg = require('./mqtt.json');
+//var mqttmsg = require('./mqtt.json');
 
 var iotdata = new AWS.IotData({endpoint: config.endpointAddress, region: 'ap-southeast-1'});
 //var iotdata = new AWS.IotData({endpoint: config.endpointAddress});
 
+// Create an SNS object
+var sns = new AWS.SNS({region: 'ap-southeast-1'});
+
 module.exports.processWL = processWL;
-processWL(mqttmsg, null);
+//processWL(mqttmsg, null);
 
 // Set up the code to call when the Lambda function is invoked
 //exports.handler = (event, context, callback) => {
@@ -30,106 +33,115 @@ function processWL(msg, context) {
     if (currWL > lastWL) {
         wlRise = true;
         alertLevel = utils.getAlertlevelRise(currWL, lastWL);
-        console.log("Level Rising ->" , alertLevel);
+        console.log("Level Rising ->", alertLevel);
     }
 
     if (currWL < lastWL) {
         wlRise = false;
         alertLevel = utils.getAlertlevelFall(currWL, lastWL);
-        console.log("Level Falling ->" , alertLevel);
+        console.log("Level Falling ->", alertLevel);
     }
 
     // May have to use history by accessing Shadow.
     if (alertLevel) {
         // Make sure that thing name is same as station id.
         // TODO : test
-        config.thingName = "hello_mpro";
-        var devState = utils.getShadowState(iotdata, config);
-        //
-/*
-        var messageText = utils.composeSMS(msg, alertLevel, wlRise, devState);
-
-        // Create an SNS object
-        var sns = new AWS.SNS({region: 'ap-southeast-1'});
-
-        var topic = config.snsTopicArn + ":" + sid;
-        // Populate the parameters for the publish operation
-        // - Message : the text of the message to send
-        // - TopicArn : the ARN of the Amazon SNS topic to which you want to publish 
-        var params2 = {
-		TopicArn: config.snsTopicArn + ":" + "MyIoTTestTopic"
-            //NextToken: 'STRING_VALUE'
-        };
+        config.thingName = "hello";
+        /*
+         var devState = utils.getShadowState(iotdata, config);
+         console.log("devState: ", devState);
+         //
+         if(devState == null) {
+         return "Error";
+         }
+         */
+        var devState;
         var subscriberList = new Array();
-        sns.listSubscriptionsByTopic(params2, function (err, data) {
-            if (err)
-                console.log(err, err.stack); // an error occurred
-            else {
-                console.log(data.Subscriptions[0].Endpoint);           // successful response
-                console.log(data.Subscriptions[1].Endpoint);
-                for (var i = 0; i < data.Subscriptions.length; i++ )
-                {
-                    subscriberList.push(data.Subscriptions[i].Endpoint)
-                }
+        iotdata.getThingShadow({
+            thingName: config.thingName
+        }, function (err, data) {
+            if (err) {
+                context.fail(err);
+                console.log("Error in getting Shadow.", err);
+            } else {
+                var jsonPayload = JSON.parse(data.payload);
+                var status = jsonPayload.state.reported.location;
+                console.log('status: ' + status);
+                devState = data;
+                var messageText = utils.composeSMS(msg, alertLevel, wlRise, devState);
+
+                var topic = config.snsTopicArn + ":" + sid;
+                var params2 = {
+                    TopicArn: config.snsTopicArn + ":" + "MyIoTTestTopic"
+                    //NextToken: 'STRING_VALUE'
+                };
+                console.log("SNS params: ", params2);
+                sns.listSubscriptionsByTopic(params2, function (err, data) {
+                    if (err)
+                        console.log(err, err.stack); // an error occurred
+                    else {
+                        console.log(data.Subscriptions[0].Endpoint);           // successful response
+                        console.log(data.Subscriptions[1].Endpoint);
+                        for (var i = 0; i < data.Subscriptions.length; i++) {
+                            subscriberList.push(data.Subscriptions[i].Endpoint)
+                        }
+                    }
+                });
             }
         });
-*/
         // TODO: Use the subscriber list to send SMS through external vendor.
-
         //sendMsg(messageText, subscriberList);
-
     } // if
 //
 }
 
+function sendMsg(msg, subsList) {
+    var user = 'TODO';
+    var pass = 'TODO';
+    var sms_from = 'BluGraph';
+    // Create a comma separared list of numbers. (max=10?)
+    var phoneList = document.write(subsList.join(", "));
+    //
+    var sms_server = 'gateway80.onewaysms.sg';
+    var path1 = "/api2.aspx?apiusername=" + user + "&apipassword=" + pass;
+    var path2 = "&message=" + encodeURI(msg) + "&languagetype=1";
 
-    function sendMsg(msg, subsList) {
-        var user = 'TODO';
-        var pass = 'TODO';
-        var sms_from = 'BluGraph';
-        // Create a comma separared list of numbers. (max=10?)
-        var phoneList = document.write(subsList.join(", "));
-        //
-        var sms_server = 'gateway80.onewaysms.sg';
-        var path1 = "/api2.aspx?apiusername=" + user + "&apipassword=" + pass;
-        var path2 = "&message=" + encodeURI(msg) + "&languagetype=1";
+    /*
+     var options = {
+     host: 'requestb.in',
+     path: '/rfyb1wrf',
+     method: 'POST'
+     };
+     */
 
-        /*
-         var options = {
-         host: 'requestb.in',
-         path: '/rfyb1wrf',
-         method: 'POST'
-         };
-         */
+    callback = function (response) {
+        var str = '';
 
-        callback = function (response) {
-            var str = '';
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
 
-            //another chunk of data has been recieved, so append it to `str`
-            response.on('data', function (chunk) {
-                str += chunk;
-            });
-
-            //the whole response has been recieved, so we just print it out here
-            response.on('end', function () {
-                console.log(str);
-                // TODO: If error, write to S3?
-            });
-        }
-
-        for (i = 0; i < subsList.length; i++) {
-            var path3 = "&senderid=" + encodeURI(sms_from) 
-                       + "&mobileno=" + encodeURI(subsList[i]);
-            var options = {
-                host: sms_server,
-                path: path1 + path2 + path3,
-                //method: 'POST'
-            };
-            var req = http.request(options, callback);
-        }
-        /*
-         req.write("hello world!");
-         req.end();
-         */
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function () {
+            console.log(str);
+            // TODO: If error, write to S3?
+        });
     }
+
+    for (i = 0; i < subsList.length; i++) {
+        var path3 = "&senderid=" + encodeURI(sms_from)
+            + "&mobileno=" + encodeURI(subsList[i]);
+        var options = {
+            host: sms_server,
+            path: path1 + path2 + path3,
+            //method: 'POST'
+        };
+        var req = http.request(options, callback);
+    }
+    /*
+     req.write("hello world!");
+     req.end();
+     */
+}
 
--- apps/lambda/alarmWL/wlAlertUtils.js
+++ apps/lambda/alarmWL/wlAlertUtils.js
@@ -5,116 +5,116 @@ var http = require('http');
 //module.exports.getAlertlevelRise = getAlertlevelRise;
 
 module.exports = {
-	getAlertlevelRise, 
-	getAlertlevelFall, 
-	getShadowState, 
-	composeSMS
+    getAlertlevelRise,
+    getAlertlevelFall,
+    getShadowState,
+    composeSMS
 };
 
-    function getAlertlevelRise(currWL, lastWL) {
-        var alertLevel = 0;
-        if ((currWL > 110) && (lastWL <= 110)) {
-            // critical.
-            alertLevel = 5;
-        }
-        if ((currWL > 100) && (lastWL <= 100)) {
-            alertLevel = 4;
-        }
-        else if ((currWL > 90) && (lastWL <= 90)) {
-            alertLevel = 3;
-        }
-        else if ((currWL > 75) && (lastWL <= 75)) {
-            alertLevel = 2;
-        }
-        else if ((currWL > 50) && (lastWL <= 50)) {
-            alertLevel = 1;
-        }
-
-        return alertLevel;
-
+function getAlertlevelRise(currWL, lastWL) {
+    var alertLevel = 0;
+    if ((currWL > 110) && (lastWL <= 110)) {
+        // critical.
+        alertLevel = 5;
+    }
+    if ((currWL > 100) && (lastWL <= 100)) {
+        alertLevel = 4;
+    }
+    else if ((currWL > 90) && (lastWL <= 90)) {
+        alertLevel = 3;
+    }
+    else if ((currWL > 75) && (lastWL <= 75)) {
+        alertLevel = 2;
+    }
+    else if ((currWL > 50) && (lastWL <= 50)) {
+        alertLevel = 1;
     }
 
-    function getAlertlevelFall(currWL, lastWL) {
-        var alertLevel = 0;
-        if ((currWL < 50) && (lastWL >= 50)) {
-            // Special value, to indicate change to normal level.
-            alertLevel = 10;
-        }
-        if ((currWL < 75) && (lastWL >= 75)) {
-            alertLevel = 1;
-        }
-        else if ((currWL < 90) && (lastWL >= 90)) {
-            alertLevel = 2;
-        }
-        else if ((currWL < 100) && (lastWL >= 100)) {
-            alertLevel = 3;
-        }
-        else if ((currWL < 110) && (lastWL >= 110)) {
-            alertLevel = 4;
-        }
+    return alertLevel;
 
-        return alertLevel;
+}
 
+function getAlertlevelFall(currWL, lastWL) {
+    var alertLevel = 0;
+    if ((currWL < 50) && (lastWL >= 50)) {
+        // Special value, to indicate change to normal level.
+        alertLevel = 10;
+    }
+    if ((currWL < 75) && (lastWL >= 75)) {
+        alertLevel = 1;
+    }
+    else if ((currWL < 90) && (lastWL >= 90)) {
+        alertLevel = 2;
+    }
+    else if ((currWL < 100) && (lastWL >= 100)) {
+        alertLevel = 3;
+    }
+    else if ((currWL < 110) && (lastWL >= 110)) {
+        alertLevel = 4;
     }
 
-    function getShadowState(iotdata, config) {
-        var currDevState;
-        console.log("Get Shadow for device: ", config);
-        iotdata.getThingShadow({
-            thingName: 'hello'
-        }, function (err, data) {
-            if (err) {
-                //context.fail(err);
-                // TODO:
-                console.log("Error in getting Shadow.", err);
-                //console.log("Request:");
-                //console.log(this.request.httpRequest);
-                //console.log("Response:");
-                //console.log(this.httpResponse);
-            } else {
-                console.log(data);
-                var jsonPayload = JSON.parse(data.payload);
-                var status = jsonPayload.state.reported.location;
-                console.log('status: ' + status);
-                currDevState = data;
+    return alertLevel;
+
+}
+
+function getShadowState(iotdata, config) {
+    var currDevState;
+    console.log("Get Shadow for device: ", config);
+    iotdata.getThingShadow({
+        thingName: config.thingName
+    }, function (err, data) {
+        if (err) {
+            //context.fail(err);
+            // TODO:
+            console.log("Error in getting Shadow.", err);
+            //console.log("Request:");
+            //console.log(this.request.httpRequest);
+            //console.log("Response:");
+            //console.log(this.httpResponse);
+            return null;
+        } else {
+            console.log(data);
+            var jsonPayload = JSON.parse(data.payload);
+            var status = jsonPayload.state.reported.location;
+            console.log('status: ' + status);
+            currDevState = data;
+            return currDevState;
+        }
+    });
+}
+
+function setShadowState(iotdata, config) {
+    var newStatus = "5 battery rd";
+    var update = {
+        "state": {
+            "desired": {
+                "location": newStatus
             }
-        });
-        return currDevState;
-    }
-
-    function setShadowState(iotdata, config) {
-        var newStatus = "5 battery rd";
-        var update = {
-            "state": {
-                "desired": {
-                    "location": newStatus
-                }
-            }
-        };
-        iotdata.updateThingShadow({
-            payload: JSON.stringify(update),
-            thingName: config.thingName
-        }, function (err, data) {
-            if (err) {
-                //context.fail(err);
-                console.log("Error in setting Shadow.")
-            } else {
-                console.log(data);
-                //context.succeed('newStatus: ' + newStatus);
-                console.log("Setting Shadow succeeded.")
-            }
-        });
-    }
-
+        }
+    };
+    iotdata.updateThingShadow({
+        payload: JSON.stringify(update),
+        thingName: config.thingName
+    }, function (err, data) {
+        if (err) {
+            //context.fail(err);
+            console.log("Error in setting Shadow.")
+        } else {
+            console.log(data);
+            //context.succeed('newStatus: ' + newStatus);
+            console.log("Setting Shadow succeeded.")
+        }
+    });
+}
 
 //
-    function composeSMS(msg, alertLevel, wlRise, devState) {
-        // Create a string extracting the click type and serial number from the message sent by the AWS IoT button
-        if (!wlRise && alertLevel == 10) {
-            // From Level 1 to normal.
-        }
-        var messageText = "Received  " + msg.ts + " message from button ID: " + msg.sid;
-        // Write the string to the console
-        console.log("Message to send: " + messageText);
-        return messageText;
+function composeSMS(msg, alertLevel, wlRise, devState) {
+    // Create a string extracting the click type and serial number from the message sent by the AWS IoT button
+    if (!wlRise && alertLevel == 10) {
+        // From Level 1 to normal.
     }
+    var messageText = "Received  " + msg.wl + " message from button ID: " + msg.sid;
+    // Write the string to the console
+    console.log("Message to send: " + messageText);
+    return messageText;
+}
