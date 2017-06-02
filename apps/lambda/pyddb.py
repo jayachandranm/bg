00f0b72479fb6e08fc23625bc33ab0ce5069731a
Jaya <jayachandranm@gmail.com>
2017-06-02 17:19:49 +0800

    Added option for getting thing shadow in Python.

--- apps/lambda/pyddb.py
+++ apps/lambda/pyddb.py
@@ -27,3 +27,10 @@ for i in response['Items']:
 while 'LastEvaluatedKey' in response:
     response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
     data.extend(response['Items'])
+
+client = boto3.client('iot-data')
+client = boto3.client('iot-data', region_name='ap-southeast-1')
+
+response = client.get_thing_shadow(
+    thingName='string'
+)
