da61e4498656ee4fad376303dcc59d505f59e082
Jaya <jayachandranm@gmail.com>
2017-06-09 17:53:41 +0800

    Return proper message through callback.

--- apps/lambda/alarmWL/index.js
+++ apps/lambda/alarmWL/index.js
@@ -3,4 +3,5 @@ var process = require('./processWLSmsExt');
 exports.handler = function (event, context, callback) {
     process.processWL(event, context);
     //callback("Daily trend updated.")
+    callback(null, "WL processed.");
 };
