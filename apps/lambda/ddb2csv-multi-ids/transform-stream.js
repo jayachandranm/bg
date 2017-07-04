4e2367355d5b39578b882ce0fe092a3eda70a8f3
Jaya <jayachandranm@gmail.com>
2017-07-04 18:35:11 +0800

    Corrected the title header for CSV daily report.

--- apps/lambda/ddb2csv-multi-ids/transform-stream.js
+++ apps/lambda/ddb2csv-multi-ids/transform-stream.js
@@ -20,7 +20,7 @@ CSVTransform.prototype._transform = function transform(JSONrecord, encoding, cal
   var self = this;
   //console.log("transform", JSONrecord);
   //var fields = ['timestamp', 'gps_data.latitude', 'gps_data.longitude', 'obd_dev_id'];
-  var fields = ['sid', 'ts', 'raw', 'wl', 'md'];
+  var fields = ['sid', 'ts', 'wa', 'md'];
   var csv = json2csv({ data: JSONrecord, fields: fields, hasCSVColumnTitle: false, del: "\t" });
   //var csv = json2csv({ data: JSONrecord, fields: fields, hasCSVColumnTitle: false, del: "," });
   this.push(csv);
