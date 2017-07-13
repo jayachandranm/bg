d2cd7353aecaefabecd9e887258209cc6015e2c2
Comfort <Jc>
2017-07-13 18:01:09 +0800

    Added template code for charting battery level, signal strenth for a given device.

---
+++ apps/web/awschart/battsJson.php
@@ -0,0 +1,22 @@
+<?php
+include 'dynDB.php';
+
+            $reqtype = 'trc';
+            $filter = new stdClass;
+
+            $filter->sid = 'CWS001'; 
+            $filter->end = round(microtime(true) *1000);
+            $filter->start = $filter->end - (3600*24*1000);
+            //print_r($filter);
+            $result = _getdata_dyndb($reqtype, $filter);
+            foreach ($result as $row) {
+                //dpm($row);
+                $time = $row['ts'];
+                $bl = $row['bl'];
+                if($bl != null) {
+                    $data[] = (object)array('date'=>$time, 'value'=>$bl);
+                    //$timestamps[] = $time;
+                }
+            } // foreach
+echo json_encode($data);
+//print_r($data);
