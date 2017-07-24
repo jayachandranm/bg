6964487700cc2156ddc8ade9410778132d0d5098
Comfort <Jc>
2017-07-24 11:25:49 +0800

    Dev in progress. Test s3 rename from prod instance.

--- apps/lambda/samples/s3rename.py
+++ apps/lambda/samples/s3rename.py
@@ -1,11 +1,30 @@
 from __future__ import print_function
 import boto3
 
-s3 = boto3.resource('s3')
+s3rc = boto3.resource('s3')
+s3client = boto3.client('s3')
 
-# List objects for each station-id. 
+with open("station-ids.json") as json_file:
+    try:
+        json_data = json.load(json_file)
+    except:
+        print("Error loading JSON file.")
+
+stations = json_data['stations']
+
+# List objects for each station-id.
 # Rearrange the name.
 # Copy and delete old.
+for sid in stations:
+    print(sid)
 
-s3.Object('my_bucket','my_file_new').copy_from(CopySource='my_bucket/my_file_old')
-s3.Object('my_bucket','my_file_old').delete()
+res = s3client.list_objects_v2(
+    Bucket='pubc5wl',
+    Delimiter='-',
+    Prefix='CWS001',
+)
+
+print(res)
+
+#s3rc.Object('my_bucket','my_file_new').copy_from(CopySource='my_bucket/my_file_old')
+#s3rc.Object('my_bucket','my_file_old').delete()
