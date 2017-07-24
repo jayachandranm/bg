96971607db20922a5ec4031f0d58cdf5af3b0c1a
Ubuntu <ubuntu@ip-10-0-0-50.ap-southeast-1.compute.internal>
2017-07-24 06:40:12 +0000

    Renames files by adjusting the names where date goes first and then converts the date value to a folder. After running the code all files directly under the folder are moved to date based folders.

--- apps/lambda/samples/s3rename.py
+++ apps/lambda/samples/s3rename.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 import boto3
+import json
 
 s3rc = boto3.resource('s3')
 s3client = boto3.client('s3')
 
-with open("station-ids.json") as json_file:
+with open("../xmlSFTPoam/station-ids.json") as json_file:
     try:
         json_data = json.load(json_file)
     except:
@@ -12,19 +13,41 @@ with open("station-ids.json") as json_file:
 
 stations = json_data['stations']
 
+bucket_name = 'pubc5wl'
+
 # List objects for each station-id.
 # Rearrange the name.
 # Copy and delete old.
-for sid in stations:
-    print(sid)
+#for sid in stations:
+#    print(sid)
 
 res = s3client.list_objects_v2(
     Bucket='pubc5wl',
     Delimiter='-',
-    Prefix='CWS001',
+    Prefix='sms_log/'
 )
 
-print(res)
+for obj in res['CommonPrefixes']:
+    prefix = obj['Prefix']
+    checking = prefix.split('/')
+    num_slash = len(checking)
+    if (num_slash < 3):
+        print("--------------------")
+        print(prefix)
+        res = s3client.list_objects_v2(
+            Bucket=bucket_name,
+            Prefix=prefix
+        )
 
-#s3rc.Object('my_bucket','my_file_new').copy_from(CopySource='my_bucket/my_file_old')
-#s3rc.Object('my_bucket','my_file_old').delete()
+        for obj in res['Contents']:
+            objkey = obj['Key']
+            once = objkey.split('/')
+            sec = once[1].split('-')
+            new = 'd' + sec[1] + '/' + sec[0] + '-' + sec[2] + '_sms.log'
+            newkey = 'sms_log/' + new
+            print(new)
+            src = bucket_name + '/' + objkey
+            s3rc.Object(bucket_name, newkey).copy_from(CopySource=src)
+            s3rc.Object(bucket_name, objkey).delete()
+
+
