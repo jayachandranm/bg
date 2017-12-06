3ed2e85973451ca225bb6bae6a6e012313a26b2d
Comfort <Jc>
2017-12-06 17:15:10 +0800

    Takes Device parameters from Shadow for creating CSV reports based on DDB data.

--- apps/lambda/monthlyReport/sid-ddb2s3-files.js
+++ apps/lambda/monthlyReport/sid-ddb2s3-files.js
@@ -7,6 +7,9 @@ var moment = require('moment');
 var DynStream = require('./dyn-stream');
 var CSVTransform = require('./transform-stream');
 var sids_j = require('./station-ids.json');
+var config = require('./config.json');
+
+var iotdata = new aws.IotData({endpoint: config.endpointAddress, region: 'ap-southeast-1'});
 
 var sids = sids_j.stations;
 
@@ -31,42 +34,56 @@ function sidRawToCsv_files(context) {
   file_dt_tag = lastMonthStart.format("MM-YYYY");
   console.log("Start/end times..", start_t, end_t, file_dt_tag);
   console.log("Number of sids..", sids.length);
-  //
+  // TODO: Report error and exit, if there is an error in processing.
   var count = 0;
   function streamToS3() {
-    if(count < sids.length) {
+    //if(count < sids.length) {
+    if(count < 2) {
       var sid = sids[count];
       count++;
       console.log("Processing, ", sid);
-      var data_stream = DynStream(table_name, sid, start_t, end_t);
-      var gzip = zlib.createGzip();
-      var csv = CSVTransform();
-      //var parser = new MyStream();
+      var devState;
+      iotdata.getThingShadow({
+        thingName: thingName
+      }, function (err, data) {
+        if (err) {
+          context.fail(err);
+          console.log("Error in getting Shadow.", err);
+        } else {
+          var jsonPayload = JSON.parse(data.payload);
+          console.log('Shadow: ' + JSON.stringify(jsonPayload, null, 2));
+          devState = jsonPayload.state.reported;
+          var data_stream = DynStream(table_name, sid, devState, start_t, end_t);
+          var gzip = zlib.createGzip();
+          var csv = CSVTransform();
+          //var parser = new MyStream();
 
-      // body will contain the compressed content to ship to s3
-      //var body = data_stream.pipe(csv).pipe(process.stdout);
-      var body = data_stream.pipe(csv).pipe(gzip);
+          // body will contain the compressed content to ship to s3
+          //var body = data_stream.pipe(csv).pipe(process.stdout);
+          var body = data_stream.pipe(csv).pipe(gzip);
 
-      var filename = sid + '_' + file_dt_tag + '.xls.gz';
-      var abs_filename = folder_name + '/' + file_dt_tag + '/' + filename;
-      console.log("Filename=", abs_filename);
+          var filename = sid + '_' + file_dt_tag + '.xls.gz';
+          var abs_filename = folder_name + '/' + file_dt_tag + '/' + filename;
+          console.log("Filename=", abs_filename);
 
-      var s3obj = new aws.S3(
-        { params:
-          { Bucket: bucket_name,
-            Key: abs_filename
-          }
-        }
-      );
-      s3obj.upload({Body: body}).
-      on('httpUploadProgress', function(evt) {
-        console.log(evt);
-      }).
-      send(function(err, data) {
-        console.log(err, data);
-      });
-      //send(function(err, data) { console.log(err, data); callback(); });
-      setTimeout(streamToS3, 500);
+          var s3obj = new aws.S3(
+            { params:
+              { Bucket: bucket_name,
+                Key: abs_filename
+              }
+            }
+          );
+          s3obj.upload({Body: body}).
+          on('httpUploadProgress', function(evt) {
+            console.log(evt);
+          }).
+          send(function(err, data) {
+            console.log(err, data);
+          });
+          //send(function(err, data) { console.log(err, data); callback(); });
+          setTimeout(streamToS3, 500);
+        } // else, iot shadow_no_err
+      }); // getThingShadow
     }// if count
   } // streamToS3
   streamToS3();
