b9db07acb583ba93eceb223ee46e0b9002c9ad79
Comfort <Jc>
2018-04-18 16:32:11 +0800

    MQTT, NodeJS, updated main function with message decoding, db update and publish reply.

--- tools/mqtt/mqtts_sub.js
+++ tools/mqtt/mqtts_sub.js
@@ -1,5 +1,11 @@
 var mqtt = require('mqtt')
 var fs = require('fs');
+var decode_rpt = require('./decode_mqtt_rpt');
+var decode_alt1 = require('./decode_mqtt_alt1');
+var decode_alt2 = require('./decode_mqtt_alt2');
+var assemble = require('./encode_mqtt');
+var dbutil = require('./dbutil');
+
 //var client  = mqtt.connect('mqtt://localhost', { host: 'localhost', port: 8883 })
 
 var deviceRoot="demo/device/"
@@ -46,5 +52,28 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log('Message:', message.toString())
+  var mqttTopic = topic;
+  var clientId = message.clientId;
+  switch(mqttTopic) {
+	  case 'rpt':
+	  var dcMsg = decode_rpt.decodeRptMessage(message);
+	  dbutil.add2dbAlerts(clientId, dcMsg);
+	  var dcMsg = decode_alt1.decodeAlt1Message(message);
+	  var replyMsg = assemble.rptReply(dcMsg);
+	  client.publish('res', replyMsg);
+	  break;
+	  case 'alt1':
+	  var dcMsg = decode_alt1.decodeAlt1Message(message);
+	  dbutil.add2dbAlerts(clientId, dcMsg);
+	  var replyMsg = assemble.alt1Reply(dcMsg);
+	  client.publish('res', replyMsg);
+	  break;
+	  case 'alt2':
+	  var dcMsg = decode_alt2.decodeAlt2Message(message);
+	  dbutil.add2dbErrors(clientId, dcMsg);
+	  var replyMsg = assemble.alt2Reply(dcMsg);
+	  client.publish('res', replyMsg);	  
+	  break;
+  }
   //client.end()
 })
