e075fe720dc4efedd0077268c429e914ec1703a1
Comfort <Jc>
2018-05-10 16:38:04 +0800

    Manage SNS, Takes in Lambda ENV variables.

--- apps/lambda/samples/manage_SNS_topics.py
+++ apps/lambda/samples/manage_SNS_topics.py
@@ -3,13 +3,12 @@ from __future__ import print_function
 import boto3
 import json
 
-#import os
-#from StringIO import StringIO
+import os
+from StringIO import StringIO
 
 #import simplejson
 #from dateutil import tz
 #from pytz import timezone
-
 
 with open("config.json") as config_json_file:
     try:
@@ -23,8 +22,6 @@ config_file = config['s3_file']
 
 s3dev_state = boto3.resource('s3')
 
-print(config_bucket, config_folder + '/' + config_file)
-
 content_object = s3dev_state.Object(config_bucket, config_folder + '/' + config_file)
 file_content = content_object.get()["Body"].read().decode('utf-8')
 dev_state_s3 = json.loads(file_content)
@@ -34,17 +31,16 @@ stations = dev_state_by_sids.keys()
 stations.sort()
 print(stations)
 
-stations = ['TST001', 'TST002']
-
 sns_client = boto3.client('sns')
-
-levels = [75, 90, 100]
-
 iot_client = boto3.client('iot-data', region_name='ap-southeast-1')
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
     #raise Exception('Something went wrong')
+    stations = ['TST001', 'TST002']
+    levels = [50, 60, 75, 90, 100]
+    stations = os.environ.get('STATIONS', stations)
+    levels = os.environ.get('RISE_LVLS', levels)
     
     data_series = []
 
@@ -54,8 +50,6 @@ def lambda_handler(event, context):
 
         for y in levels:
             topic_name = x + "-" + str(y)
-
             print(topic_name)
-
             response = sns_client.create_topic( Name=topic_name )
 
