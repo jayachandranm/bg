734a08214b7fc19d3e7746b9916a04af45594420
Comfort <Jc>
2018-05-17 12:04:37 +0800

    MQTT, adding config template.

---
+++ tools/mqtt/config_tmpl.js
@@ -0,0 +1,14 @@
+module.exports = config;
+
+var config = {};
+
+config.mysql = {};
+
+config.mysql.db = 'mydb';
+config.mysql.dbhost = 'localhost';
+config.mysql.user = 'user';
+config.mysql.pass = 'pass';
+
+config.smsUser = 'user';
+config.smsPass = 'pass';
+config.smsFrom = 'from';
