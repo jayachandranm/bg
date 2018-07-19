0a375d1a5bdb73632a2d1f67e534d4697b66d84b
Jaya M <jayachandranm@gmail.com>
2018-07-19 10:04:48 +0800

    JAV web, remove title for content type with preprocess_node in template php file.

--- apps/web/drupal/themes/jav/template.php
+++ apps/web/drupal/themes/jav/template.php
@@ -8,3 +8,11 @@ function hdbjav_preprocess_block(&$variables) {
     //dpm($variables);
 }
 
+/**
+ * Implements hook_preprocess_node
+ */
+function hdbjav_preprocess_node(&$variables){
+  if ($variables['node']->type == 'lift_companies') {
+    $variables['title'] = FALSE;
+  }
+}
