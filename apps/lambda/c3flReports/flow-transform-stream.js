23e75a3a789c7b5ceb8f149d6248ab271f3fb29a
Jaya M <jayachandranm@gmail.com>
2019-02-01 12:39:17 +0800

    Added new dir and files for C3 FL monthly reports.

---
+++ apps/lambda/c3flReports/flow-transform-stream.js
@@ -0,0 +1,30 @@
+var Transform = require('stream').Transform;
+var inherits = require('util').inherits;
+var json2csv = require('json2csv');
+//var jsonexport = require('jsonexport');
+
+module.exports = CSVTransform;
+
+function CSVTransform(options) {
+  if ( ! (this instanceof CSVTransform))
+    return new CSVTransform(options);
+
+  if (! options) options = {};
+  options.objectMode = true;
+  Transform.call(this, options);
+}
+
+inherits(CSVTransform, Transform);
+
+CSVTransform.prototype._transform = function transform(JSONrecord, encoding, callback) {
+  var self = this;
+  //console.log("transform", JSONrecord);
+  //var fields = ['timestamp', 'gps_data.latitude', 'gps_data.longitude', 'obd_dev_id'];
+  //var fields = ['sid', 'ts', 'wa', 'md'];
+  var fields = ['dt', 'sg', 'wa', 'vl', 'fl', 'md'];
+  //var csv = json2csv({ data: JSONrecord, fields: fields, hasCSVColumnTitle: false, del: "\t" });
+  var csv = json2csv({ data: JSONrecord, fields: fields, hasCSVColumnTitle: false, del: "," });
+  this.push(csv);
+  this.push("\n");
+  callback();
+};
---
+++ apps/lambda/c3flReports/rlevel-monthly-report.js
@@ -0,0 +1,134 @@
+var aws = require('aws-sdk');
+var stream = require('stream');
+//var MyStream = require('json2csv-stream');
+var zlib = require('zlib');
+//var dateFormat = require('dateformat');
+var moment = require('moment');
+var archiver = require('archiver');
+var DynStream = require('./rlevel-dyn-stream');
+var CSVTransform = require('./rlevel-transform-stream');
+//var sids_json = require('./station-B-ids.json');
+var config = require('./config_rlevel.json');
+
+var iotdata = new aws.IotData({endpoint: config.endpointAddress, region: 'ap-southeast-1'});
+
+var sids; //sids_json.stations;
+var devs_state;
+
+exports.handler = function (event, context) {
+    //
+    console.log("Loading..");
+    sidRawToCsv(context);
+  };
+
+function sidRawToCsv(context) {
+  //function backupTable(tablename, callback) {
+  //console.log("backup..");
+  var table_name = config.table;
+  var bucket_name = config.bucket;
+  var folder_name = config.folder;
+  var dev_state_file = config.dev_file;
+  //
+  var iot_folder_name = config.folder_iot;
+
+  //
+  // moment().local();
+  //var ts = dateFormat(new Date(), "mmddyyyy-HHMMss");
+  var currMonthStart = moment().utcOffset('+0800').startOf('month');
+  var lastMonthStart = moment(currMonthStart).subtract(1, 'months');
+  var lastMonthEnd = moment(lastMonthStart).endOf('month');
+  //console.log(currMonthStart.format(), lastMonthStart.format(), lastMonthEnd.format());
+  //console.log(currMonthStart.valueOf(), lastMonthStart.valueOf(), lastMonthEnd.valueOf())
+  var end_t = lastMonthEnd.valueOf();
+  var start_t = lastMonthStart.valueOf();
+  //self._start_t = start_t;
+  var file_dt_tag = lastMonthStart.format("YYYYMM");
+  console.log("Start/end times..", start_t, end_t, file_dt_tag);
+  //
+  var archive = archiver('zip');
+  archive.on('error', function(err) {
+    throw err;
+  });
+  //
+  var s3obj = new aws.S3(
+   { params:
+     { Bucket: bucket_name,
+       Key: folder_name + '/' + file_dt_tag + '_flow.zip'
+     }
+   }
+  );
+  //
+  s3obj.upload({Body: archive}).
+  on('httpUploadProgress', function(evt) {
+    console.log(evt);
+  }).
+  send(function(err, data) {
+    console.log(err, data);
+  });
+
+  //
+  var s3dev = new aws.S3();
+  var params_dev = 
+     { Bucket: bucket_name,
+       Key: iot_folder_name + '/' + dev_state_file
+     };
+
+  s3dev.getObject(params_dev, function(err, data) {
+    var fileContents = data.Body.toString();
+    devs_state = JSON.parse(fileContents);
+    var dev_b_state_by_sids = devs_state.dev_state;
+    sids = Object.keys(dev_b_state_by_sids);
+    //console.log(sids);
+    //
+    getMultiFileStream(sids);
+  });
+
+
+  var count = 0;
+  function getMultiFileStream(callback) {
+    // sids defined as global, so that the variable is available for,
+    // repeated call  of this function.
+    if(count < sids.length) {
+      var sid = sids[count];
+      count++;
+      console.log("Processing, ", sid);
+      var devState;
+      iotdata.getThingShadow({
+        thingName: sid
+      }, function (err, data) {
+        if (err) {
+          context.fail(err);
+          console.log("Error in getting Shadow.", err);
+        } else {
+          var jsonPayload = JSON.parse(data.payload);
+          //console.log('Shadow: ' + JSON.stringify(jsonPayload, null, 2));
+          devState = jsonPayload.state.reported;
+          var cl = devs_state.dev_state[sid].critical_level;
+          devState.critical_level = cl;
+          var data_stream = DynStream(table_name, sid, devState, start_t, end_t);
+          var gzip = zlib.createGzip();
+          var csv = CSVTransform();
+
+          // body will contain the stream content to ship to s3
+          //var body = data_stream.pipe(csv).pipe(process.stdout);
+          //var body = data_stream.pipe(csv).pipe(gzip);
+          var body = data_stream.pipe(csv);
+
+          var filename = sid + '_' + file_dt_tag + '.csv';
+          //var abs_filename = folder_name + '/' + file_dt_tag + '/' + filename;
+          //console.log("Filename=", abs_filename);
+          archive.append(body, { name: filename });
+          //send(function(err, data) { console.log(err, data); callback(); });
+          setTimeout(getMultiFileStream, config.pause);
+        }
+      }); // getThingShadow
+    }// if count
+    else{
+      console.log("All stations processed.");
+      archive.finalize();
+    }
+  } // getMultiFileStream
+  //getMultiFileStream();
+} // sidRawToCsv
+
+module.exports.sidRawToCsv = sidRawToCsv;
