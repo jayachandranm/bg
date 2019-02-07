529300e4f27c6585b6859482e0ab033a9f169a98
Jaya M <jayachandranm@gmail.com>
2019-02-07 22:28:54 +0800

    Transform, added offset to wa to get wh and adds wh to DDB. Modifies offset for all sensors except rain sensor.

--- apps/lambda/c3flTransform/transformFlow.js
+++ apps/lambda/c3flTransform/transformFlow.js
@@ -36,7 +36,7 @@ function transformFlow(event, context, callback) {
 
     var sid = msg.sid;
     //var thingName = sid;
-    var flSType = Number(msg.ty);
+    var sensorType = Number(msg.ty);
     var ts_unix = msg.ts;
     var ts_r = msg.ts_r;
     // TODO: Check default value.
@@ -52,11 +52,11 @@ function transformFlow(event, context, callback) {
     var newmsg = {};
     newmsg.sid = sid;
     newmsg.ts = ts_unix;
-    newmsg.ty = flSType;
+    newmsg.ty = sensorType;
     newmsg.ts_r = msg.ts_r;
     //
     // Convert to appropriate scales.
-    newmsg.wa = wa;
+    //newmsg.wa = wa;
     newmsg.vl = vl;
     newmsg.wt = msg.wt === undefined ? undefined : Number(msg.wt);
     newmsg.snr = msg.snr === undefined ? undefined : Number(msg.snr);
@@ -76,12 +76,11 @@ function transformFlow(event, context, callback) {
     }
     */
 
-
     // If md is defined, keep it in the DDB, else no field in the DDB.
     //if (msg.hasOwnProperty(md) ) { // && msg.md !== null) {
     //if (typeof (msg.md) !== 'undefined') { // && msg.md !== null) {
     var md = msg.md === undefined ? '0' : msg.md;
-    if (md !== '0') { 
+    if (md !== '0') {
         var logMsg = "Maintenance mode."
         console.log(logMsg, eventText);
         newmsg.md = "maintenance";
@@ -89,40 +88,57 @@ function transformFlow(event, context, callback) {
         //addToDDBexit(tablename, newmsg, callback);
     }
 
-    // If fl is defined,
-    // TODO: handle wa undefined.
-    if (typeof (fl) !== 'undefined') {
-        // Modify fl as required.
-        if (flSType != 2) {
-            var logMsg = "Sensor Type is not 2, nothing to transform, write to DDB.";
-            console.log(logMsg);
-            console.log("Updated msg=", newmsg);
-            addToDDBexit(tablename, newmsg, callback);
-            //context.success();
-            //callback(null, logMsg);
-        }
-        else if (wa == 0) {
-            var logMsg = "Water level 0, set fl to 0, write to DDB.";
-            console.log(logMsg);
-            newmsg.fl = 0;
-            console.log("Updated msg=", newmsg);
-            addToDDBexit(tablename, newmsg, callback);
+    //let name = event.name === undefined ? 'you' : event.name;
+    var devState;
+    iotdata.getThingShadow({
+        thingName: sid
+    }, function (err, data) {
+        if (err) {
+            context.fail(err);
+            console.log("Error in getting Shadow.", err);
         } else {
-            //let name = event.name === undefined ? 'you' : event.name;
-            var devState;
+            // TODO: check that shadow is not empty.
+            var jsonPayload = JSON.parse(data.payload);
+            console.log('Shadow: ' + JSON.stringify(jsonPayload, null, 2));
+            devState = jsonPayload.state.reported;
+            var offset = devState.offset === undefined ? 0 : devState.offset;
+            var cope = devState.cope_level === undefined ? 0 : devState.cope_level;
+            var invert = devState.invert_level === undefined ? 0 : devState.invert_level;
+
+            var wh = 0;
+            if (sensorType == 1 || sensorType == 2) {
+                wh = wa + offset;
+            } else if (sensorType == 3) {
+                // Depth sensor.
+                wh = cope + offset - newmsg.wr;
+            }
+            else {
+                // Rain sensor.
+                wh = undefined;
+            }
             //
-            iotdata.getThingShadow({
-                thingName: sid
-            }, function (err, data) {
-                if (err) {
-                    context.fail(err);
-                    console.log("Error in getting Shadow.", err);
+            newmsg.wh = wh;
+
+            // If fl is defined,
+            // TODO: handle wa undefined.
+            if (typeof (fl) !== 'undefined') {
+                // Modify fl as required.
+                if (sensorType != 2) {
+                    var logMsg = "Sensor Type is not 2, nothing to transform, write to DDB.";
+                    console.log(logMsg);
+                    console.log("Updated msg=", newmsg);
+                    addToDDBexit(tablename, newmsg, callback);
+                    //context.success();
+                    //callback(null, logMsg);
+                }
+                else if (wa == 0) {
+                    var logMsg = "Water level 0, set fl to 0, write to DDB.";
+                    console.log(logMsg);
+                    newmsg.fl = 0;
+                    console.log("Updated msg=", newmsg);
+                    addToDDBexit(tablename, newmsg, callback);
                 } else {
-                    var jsonPayload = JSON.parse(data.payload);
-                    console.log('Shadow: ' + JSON.stringify(jsonPayload, null, 2));
-                    devState = jsonPayload.state.reported;
-                    var offset = devState.offset === undefined ? 0 : devState.offset;
-                    var wh = wa + offset;
+                    //
                     //var ct = devState.ct === undefined ? "generic" : devState.ct;
                     if (devState.ct === undefined || devState.ct === "generic") {
                         var h1 = devState.h1 === undefined ? 0 : devState.h1;
@@ -148,11 +164,12 @@ function transformFlow(event, context, callback) {
                         console.log("Updated msg=", newmsg);
                         addToDDBexit(tablename, newmsg, callback);
                     }
-                }
-            }); // getThingShadow
-        } // if 
-    }
-}
+                } // if st=2, else..
+            } // if fl sensor.
+        } // if shadow success
+    }); // getThingShadow
+} // transformFlow
+
 
 function addToDDBexit(tablename, msg, callback) {
     var params = {
